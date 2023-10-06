ce0cf5b73273d10dd1fb5bdfc4ffb9e3139db8bc
Mario Hernandez <mhmario963@gmail.com>
2023-10-06 14:46:57 -0400

    implement concurrent updates to the ui thread using channels.

--- intro-coroutines/src/tasks/Request7Channels.kt
+++ intro-coroutines/src/tasks/Request7Channels.kt
@@ -1,9 +1,11 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import java.util.concurrent.CountDownLatch
 
 suspend fun loadContributorsChannels(
     service: GitHubService,
@@ -11,6 +13,30 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
+
+        val channel = Channel<List<User>>()
+
+        repos.map { repo ->
+            launch {
+                val users = service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(users)
+            }
+        }
+
+        var allUsers = emptyList<User>()
+
+        repeat(repos.size) { index ->
+            allUsers = (allUsers + channel.receive()).aggregate()
+            updateResults(
+                allUsers,
+                index == repos.lastIndex
+            )
+        }
     }
 }
